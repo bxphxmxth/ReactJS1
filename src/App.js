0c14a0b2c770577eb53913c8d44e086fbac35279
bxphxmxth <fushs@hotmail.fr>
2021-06-21 18:29:42 +0200

    footer

--- src/App.js
+++ src/App.js
@@ -1,23 +1,28 @@
 import logo from './logo.svg';
 import './App.css';
+import DemoComponent from "./components/demoComponent";
+import Footer from "./components/footer/footer";
 
 function App() {
+
+  let dateCourante = new Date();
+  let tempsJavaScript = dateCourante.getTime();
+  let tempsPHP = tempsJavaScript/1000
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <DemoComponent/>
+        
+        <p>Temps JavaScript: {tempsJavaScript} en milliseconde</p>
+
+
+        <p>Temps PHP: {tempsPHP} en milliseconde</p>
+
+        <p>Date pour un humain: {dateCourante.getDate()}/{dateCourante.getMonth()}/{dateCourante.getFullYear()}</p>
       </header>
+      <Footer/>
     </div>
   );
 }
---
+++ src/components/demoComponent.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class DemoComponent extends React.Component{
+     render(){
+          return(
+               <div>
+                    hellow world
+               </div>
+          )
+     }
+}
+
+export default DemoComponent
